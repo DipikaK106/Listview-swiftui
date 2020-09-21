5f0987ba6dc61cab97ab30628f71c314261268f0
dipika <b.dipika106@gmail.com>
2020-09-22 09:47:16 +1000

    Initial Commit

---
+++ Listview-swiftui/Listview_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Listview_swiftuiApp.swift
+//  Listview-swiftui
+//
+//  Created by Dipika on 22/9/20.
+//
+
+import SwiftUI
+
+@main
+struct Listview_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
